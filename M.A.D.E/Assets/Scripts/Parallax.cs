49346145e1ddfe0d2f714933573e5283b0c0470c
Adrián <rigo.imre.adrian007@gmail.com>
2026-02-10 22:17:07 +0100

    parallax script, lvl map

---
+++ M.A.D.E/Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEffect; 
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+    }
+}
