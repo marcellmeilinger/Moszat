ded28f9b324e1c6c443390a4f6508b776c88d61a
Patai Péter Erik <erikppatai@gmail.com>
2026-04-20 20:29:56 +0200

    Save before the update

--- M.A.D.E/Assets/Scripts/GateInteraction.cs
+++ M.A.D.E/Assets/Scripts/GateInteraction.cs
@@ -1,69 +1,95 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
-/// <summary>
-/// Kezeli a kapukkal való interakciót a játékos részéről.
-/// Figyeli a játékos közelségét és a gombnyomást ('E') az animáció elindításához.
-/// </summary>
 public class GateInteraction : MonoBehaviour
 {
-    /// <summary>
-    /// A kapu animációiért felelős komponens referenciája.
-    /// </summary>
+    public int requiredCoins = 100;
+
+    [Header("UI Visszajelzés")]
+    public TextMeshProUGUI feedbackText;
+    public float textDisplayTime = 3f;
+
     private Animator anim;
-
-    /// <summary>
-    /// Igaz, ha a játékos a kapuhoz tartozó Trigger területen belül tartózkodik.
-    /// </summary>
     private bool isPlayerNear;
+    private bool isOpened = false;
+    private PlayerWallet playerWallet;
 
-    /// <summary>
-    /// Jelzi, hogy a kapu meg lett-e már nyitva.
-    /// </summary>
-    private bool isOpened;
-
-    /// <summary>
-    /// Unity Start metódus, Inicializálja az Animator komponenst.
-    /// </summary>
     void Start()
     {
         anim = GetComponent<Animator>();
-    }
 
-    /// <summary>
-    /// Képkockánként lefutó frissítés.
-    /// Ellenőrzi, hogy a játékos a közelben van-e, a kapu még zárva van, 
-    /// és lenyomta-e a megfelelő gombot ('E') a nyitáshoz.
-    /// </summary>
-    void Update()
-    {
-        if (isPlayerNear && !isOpened && Input.GetKeyDown(KeyCode.E))
+        if (feedbackText != null)
         {
-            anim.SetTrigger("Open");
-            isOpened = true;
+            feedbackText.text = "";
         }
     }
 
-    /// <summary>
-    /// Akkor hívódik meg, ha egy másik 2D fizikai objektum belép a trigger zónába.
-    /// </summary>
-    /// <param name="collision">Az ütközésben részt vevő Collider adatai.</param>
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpened)
+            {
+                if (playerWallet != null)
+                {
+                    if (playerWallet.currentCoins >= requiredCoins)
+                    {
+                        anim.SetTrigger("Open");
+                        isOpened = true;
+                        ShowFeedback("The gate has opened!\nPress 'E' again to enter.");
+                    }
+                    else
+                    {
+                        int missingCoins = requiredCoins - playerWallet.currentCoins;
+                        ShowFeedback("<sprite=0>" + missingCoins);
+                    }
+                }
+            }
+            else
+            {
+                if (LevelManager.Instance != null)
+                {
+                    LevelManager.Instance.NextLevel();
+                }
+            }
+        }
+    }
+
+    private void ShowFeedback(string message)
+    {
+        if (feedbackText != null)
+        {
+            StopAllCoroutines();
+            StartCoroutine(DisplayText(message));
+        }
+        Debug.Log(message);
+    }
+
+    IEnumerator DisplayText(string message)
+    {
+        feedbackText.text = message;
+        yield return new WaitForSeconds(textDisplayTime);
+        feedbackText.text = "";
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             isPlayerNear = true;
+            playerWallet = collision.GetComponent<PlayerWallet>();
         }
     }
 
-    /// <summary>
-    /// Akkor hívódik meg, ha egy másik 2D fizikai objektum elhagyja a trigger zónát.
-    /// </summary>
-    /// <param name="collision">A trigger zónát elhagyó Collider adatai.</param>
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             isPlayerNear = false;
+            playerWallet = null;
+
+            if (feedbackText != null) feedbackText.text = "";
         }
     }
 }
